d9e38da0f5c498b615ca9b8a1312b89a01923a68
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2016-02-02 14:49:20 +0100

    NF

---
+++ gen_drops_staging_area.sql
@@ -0,0 +1,138 @@
+  DECLARE
+  /* CURSOR QUE NOS DARA TODAS LAS TABLAS QUE HAY QUE CREAR EN EL STAGING AREA */
+  CURSOR dtd_interfaz_summary
+  IS
+    SELECT 
+      TRIM(CONCEPT_NAME) "CONCEPT_NAME",
+      SOURCE,
+      INTERFACE_NAME,
+      TYPE,
+      SEPARATOR,
+      DELAYED
+    FROM MTDT_INTERFACE_SUMMARY;
+    
+  CURSOR dtd_interfaz_summary_history
+  IS
+    SELECT 
+      TRIM(CONCEPT_NAME) "CONCEPT_NAME",
+      SOURCE,
+      INTERFACE_NAME,
+      TYPE,
+      SEPARATOR,
+      DELAYED,
+      HISTORY
+    FROM MTDT_INTERFACE_SUMMARY
+    where HISTORY is not null;
+    
+  
+
+      reg_summary dtd_interfaz_summary%rowtype;
+      reg_summary_history dtd_interfaz_summary_history%rowtype;
+      
+
+      
+      primera_col INTEGER;
+      TYPE list_columns_primary  IS TABLE OF VARCHAR(30);
+      TYPE list_columns_partitioned  IS TABLE OF VARCHAR(30);
+      TYPE list_tablas_RE IS TABLE OF VARCHAR(30);
+
+      lista_pk                                      list_columns_primary := list_columns_primary (); 
+      tipo_col                                      VARCHAR(70);
+      lista_par                                     list_columns_partitioned := list_columns_partitioned();
+      v_lista_tablas_RE                        list_tablas_RE := list_tablas_RE();
+      lista_campos_particion            VARCHAR(250);
+      no_encontrado                          VARCHAR(1);
+      subset                                         VARCHAR(1);
+      OWNER_SA                             VARCHAR2(60);
+      OWNER_T                                VARCHAR2(60);
+      OWNER_DM                            VARCHAR2(60);
+      OWNER_MTDT                       VARCHAR2(60);
+      TABLESPACE_SA                  VARCHAR2(60);
+      v_existe_tablas_RE integer:=0;
+      v_encontrado VARCHAR2(1):='N';
+
+      
+
+
+BEGIN
+  /* (20150119) ANGEL RUIZ*/
+  /* ANTES DE NADA LEEMOS LAS VAR. DE ENTORNO PARA TIEMPO DE GENERACION*/
+  SELECT VALOR INTO OWNER_SA FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_SA';
+  SELECT VALOR INTO OWNER_T FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_T';
+  SELECT VALOR INTO OWNER_DM FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_DM';
+  SELECT VALOR INTO TABLESPACE_SA FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'TABLESPACE_SA';
+  /* (20150119) FIN*/
+
+  /* (20151117) Angel Ruiz. NF. Generacion de los creates de tablas SAD y SADH*/
+  FOR nombre_tabla_HF in (
+      SELECT distinct substr(table_name, 4) nombre_tabla
+      FROM MTDT_TC_SCENARIO
+      WHERE TABLE_TYPE = 'I'
+      AND REINYECTION = 'Y')
+  LOOP
+    v_existe_tablas_RE:=1;
+    v_lista_tablas_RE.EXTEND;
+    v_lista_tablas_RE (v_lista_tablas_RE.last) := nombre_tabla_HF.nombre_tabla;
+  END LOOP;
+  /* (20151117) Angel Ruiz. FIN NF. Generacion de los creates de tablas SAD y SADH*/
+
+  --DBMS_OUTPUT.put_line('set echo on;');
+  --DBMS_OUTPUT.put_line('whenever sqlerror exit 1;');
+  DBMS_OUTPUT.put_line('');
+  OPEN dtd_interfaz_summary;
+  LOOP
+    FETCH dtd_interfaz_summary
+      INTO reg_summary;
+      EXIT WHEN dtd_interfaz_summary%NOTFOUND;  
+      DBMS_OUTPUT.put_line('DROP TABLE ' || 'SA_' || reg_summary.CONCEPT_NAME || ' CASCADE;');
+      /* (20151118) Angel Ruiz. NF: Creacion de tablas para inyeccion SAD */
+      if v_existe_tablas_RE = 1 then
+        /* Existen tablas de inyeccion */
+        v_encontrado:='N';
+        for indx in v_lista_tablas_RE.FIRST .. v_lista_tablas_RE.LAST
+        loop
+          if (v_lista_tablas_RE(indx) = reg_summary.CONCEPT_NAME) then
+            v_encontrado := 'Y';
+          end if;
+        end loop;
+        if v_encontrado = 'Y' then
+          DBMS_OUTPUT.put_line('DROP TABLE ' || 'SAD_' || reg_summary.CONCEPT_NAME || ' CASCADE;');
+        end if;
+      end if;
+      /* (20151118) Angel Ruiz. FIN NF: Creacion de tablas para inyeccion SAD */
+
+  END LOOP;
+  CLOSE dtd_interfaz_summary;
+  /****************************************************************/
+  /* (20150717) ANGEL RUIZ. NUEVA FUNCIONALIDAD.*/
+  /* Las tablas de STAGING pueden tener HISTORICO */
+  /*****************************************************************************/  
+  OPEN dtd_interfaz_summary_history;
+  LOOP
+    FETCH dtd_interfaz_summary_history
+      INTO reg_summary_history;
+      EXIT WHEN dtd_interfaz_summary_history%NOTFOUND;  
+      DBMS_OUTPUT.put_line('DROP TABLE ' || 'SAH_' || reg_summary_history.CONCEPT_NAME || ' CASCADE;');
+      /* (20151118) Angel Ruiz. NF: Creacion de tablas para inyeccion SAD */
+      if v_existe_tablas_RE = 1 then
+        /* Existen tablas de inyeccion */
+        v_encontrado:='N';
+        for indx in v_lista_tablas_RE.FIRST .. v_lista_tablas_RE.LAST
+        loop
+          if (v_lista_tablas_RE(indx) = reg_summary_history.CONCEPT_NAME) then
+            v_encontrado := 'Y';
+          end if;
+        end loop;
+        if v_encontrado = 'Y' then
+          DBMS_OUTPUT.put_line('DROP TABLE ' || 'SADH_' || reg_summary_history.CONCEPT_NAME || ' CASCADE;');
+        end if;
+      end if;
+      /* (20151118) Angel Ruiz. FIN NF: Creacion de tablas para inyeccion SAD */
+      
+  END LOOP;
+  CLOSE dtd_interfaz_summary_history;
+  DBMS_OUTPUT.put_line('');
+  --DBMS_OUTPUT.put_line('set echo off;');
+  --DBMS_OUTPUT.put_line('exit SUCCESS;');
+END;
+
